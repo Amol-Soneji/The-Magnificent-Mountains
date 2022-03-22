7a4c9dc72e12ee522be100ff15363c10077c459f
Amol Soneji <eon747@live.com>
2022-03-22 03:22:04 -0400

    Added some files.

--- User/internalUserModf.h
+++ User/internalUserModf.h
@@ -1,12 +1,20 @@
 #ifndef INTERNAL_USER_MODIFICATION_H
 #define INTERNAL_USER_MODIFICATION_H
 
-#include <utility>
 #include <userCharacter.h>
 #include <inventory.h>
 #include <user.h>
 
-using std::pair;
+class InternalUserModification
+{
+    public:
+    InternalUserModification(User* userPointer, Inventory* inventoryPointer, UserCharacter* usrCharPointer) : userPointer(userPointer), inventoryPointer(inventoryPointer), usrCharPointer(usrCharPointer) {}
+
+    private:
+        User* userPointer;
+        Inventory* inventoryPointer;
+        UserCharacter* usrCharPointer;
+};
 
 #endif // !INTERNAL_USER_MODIFICATION_H
 
--- User/user.h
+++ User/user.h
@@ -2,15 +2,25 @@
 #define USER_H
 
 #include <utility>
+#include <string>
 
 using std::pair;
+using std::string;
 
 class User
 {
 	public:
+    User(currentLoaction, userName, isBanned) : currentLoaction(currentLoaction), userName(userName), isBanned(isBanned) {}
+    pair<int, int> getCurrentLocation() const;
+    string getUserName() const;
+    bool getIsBanned() const;
+    void changeCurrentLocation(pair<int, int> newLocation);
+    //void checkBanStatus();
 
 	private:
 		pair<int, int> currentLoaction;
+        string userName;
+        bool isBanned;
 };
 
 #endif // !USER_H
